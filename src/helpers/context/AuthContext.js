c05d5b0f3edcff6c25e15408b7fd9a4cfe2609fe
thehieuduong7 <hieuduong959@gmail.com>
2022-11-19 22:41:24 +0700

    auth context

--- src/helpers/context/AuthContext.js
+++ src/helpers/context/AuthContext.js
@@ -1,10 +1,13 @@
 import authReducer, { initAuthState, authAction } from "../feature/authSlice";
 import { createContext, useReducer, useEffect, useState, useRef } from "react";
 import { authService } from "../service/authService";
+import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext();
 
 function AuthContextProvider({ children }) {
+	const [customerId, setCustomerId] = useState()
 	const [authState, dispatch] = useReducer(authReducer, initAuthState);
+	const nagivate =useNavigate()
 	const [alertAuth, setAlertAuth] = useState({
 		show: false,
 		type: "error",
@@ -19,6 +22,7 @@ function AuthContextProvider({ children }) {
 	};
 	const setAlertFail = ({ message }) => {
 		if (timeout) {
+			console.log("clear", timeout.current);
 			clearTimeout(timeout.current);
 		}
 		setAlertAuth({
@@ -32,18 +36,19 @@ function AuthContextProvider({ children }) {
 	};
 
 	const loginUser = async (userForm) => {
-		dispatch(authAction.setLoadingAuth(true));
+		dispatch(authAction.loadingAuth());
 		try {
-			await authService.login(userForm);
-			await loadingUser();
+			const res = await authService.login(userForm);
+			return res;
 		} catch (err) {
-			setAlertFail(err);
-			dispatch(authAction.setLoadingAuth(false));
+			return err;
+		} finally {
+			loaddingUser();
 		}
 	};
 
-	const loadingUser = async () => {
-		dispatch(authAction.setLoadingAuth(true));
+	const loaddingUser = async () => {
+		dispatch(authAction.loadingAuth());
 		try {
 			const res = await authService.loadUser();
 			dispatch(
@@ -53,12 +58,16 @@ function AuthContextProvider({ children }) {
 			);
 			return res;
 		} catch (err) {
-			dispatch(authAction.setAuth({}));
+			dispatch(
+				authAction.setAuth({
+					user: null,
+				})
+			);
 			return err;
 		}
 	};
 	const registerUser = async (userForm) => {
-		authAction.setLoadingAuth(true);
+		authAction.loadingAuth();
 		try {
 			const res = await authService.register(userForm);
 			return res;
@@ -66,18 +75,21 @@ function AuthContextProvider({ children }) {
 			return err;
 		}
 	};
-	const logout = () => {
-		authService.logout();
-		dispatch(authAction.logout());
+	const logout = async () => {
+		await authService.logout()
+		dispatch(authAction.logout())
+		nagivate("/")
+
 	};
 	useEffect(() => {
-		loadingUser();
+		loaddingUser();
 	}, []);
 	const authContextData = {
+		customerId, setCustomerId,
 		authState,
 		dispatch,
 		loginUser,
-		loadingUser,
+		loaddingUser,
 		registerUser,
 		logout,
 		alertAuth,
